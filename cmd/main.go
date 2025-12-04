34255801f53e09f9730c036d2df834ef1f4362a6
Vincent Handoko <vincenthandoko288@gmail.com>
2025-12-04 18:50:16 +0700

    Add Create Post

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,11 @@ import (
 	"github.com/VH288/miniig/internal/configs"
 
 	"github.com/VH288/miniig/internal/handlers/memberships"
+	"github.com/VH288/miniig/internal/handlers/posts"
 	membershipRepo "github.com/VH288/miniig/internal/repository/memberships"
+	postRepo "github.com/VH288/miniig/internal/repository/posts"
 	membershipSvc "github.com/VH288/miniig/internal/service/memberships"
+	postSvc "github.com/VH288/miniig/internal/service/posts"
 	"github.com/VH288/miniig/pkg/internalsql"
 	"github.com/gin-gonic/gin"
 )
@@ -32,13 +35,23 @@ func main() {
 	if err != nil {
 		log.Fatal("Gagal inisiai database", err)
 	}
+
 	membershipRepo := membershipRepo.NewRepository(db)
 	membershipService := membershipSvc.NewService(cfg, membershipRepo)
 
+	postRepo := postRepo.NewRepository(db)
+	postSvc := postSvc.NewService(cfg, postRepo)
+
 	r := gin.Default()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
 
 	membershipHandler := memberships.NewHandler(r, membershipService)
 	membershipHandler.RegisterRoute()
+
+	postHandler := posts.NewHandler(r, postSvc)
+	postHandler.RegisterRoute()
 
 	err = r.Run(cfg.Service.Port)
 	if err != nil {
